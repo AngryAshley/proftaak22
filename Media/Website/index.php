e2788ceef2f177d1be1e24367ccdbe5e921f67cb
CarloKessel <carlovankessel@yahoo.nl>
2021-11-05 15:15:38 +0100

    Update index.php

--- Media/Website/index.php
+++ Media/Website/index.php
@@ -11,7 +11,6 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
-    <script src="js/script.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RailView</title>
 </head>
@@ -50,3 +49,5 @@
     </div>
 </body>
 </html>
+
+<script src="js/script.js"></script>
