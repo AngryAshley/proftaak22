74746a231a6f584a3aeef47222c749c4ee8914e9
Nick Welles <nickwelles@live.nl>
2021-11-23 15:37:30 +0100

    update gitignore with ASP.NET core. add database GUI ASP.NET project

---
+++ Software/RailView_database_GUI/RailView_database_GUI_ASP/RailView_database_GUI_ASP/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RailView_database_GUI_ASP.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
