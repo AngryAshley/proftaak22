bc1fa5835cea8eecefb14abbb0ee2c738b0436da
Ben <bendevriesxd@gmail.com>
2021-11-25 15:30:30 +0100

    The website is now succesfully connected to the database. It also shows data (notifications) from the database.

---
+++ Software/RailViewClient_ASP/RailViewClient/RailViewClient/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using RailViewClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RailViewClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        //string serverName = "192.168.161.205";
+        //string userName = "admin";
+        //string passWord = "TopMaster99";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Alert> alerts = new List<Alert>();
+
+            //Connect to Mysql
+            using (MySqlConnection con = new MySqlConnection("Server=192.168.161.205;Port=3306;Database=RailView;Uid=admin;Pwd=TopMaster99;"))
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from alerts", con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Alert alert = new Alert();
+                    alert.id = Convert.ToInt32(reader["id"]);
+                    alert.alert = reader["alert"].ToString();
+                    alert.route = reader["route"].ToString();
+                    alert.times = reader["times"].ToString();
+
+                    alerts.Add(alert);
+                }
+                reader.Close();
+                con.Close();
+            }
+
+            return View(alerts);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Software/RailViewClient_ASP/RailViewClient/RailViewClient/Models/Alert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RailViewClient.Models
+{
+    public class Alert
+    {
+        public int id { get; set; }
+
+        public int cam_Id { get; set; }
+
+        public string alert { get; set; }
+
+        public double location_X { get; set; }
+
+        public double location_Y { get; set; }
+
+        public string route { get; set; }
+
+        public string times { get; set; }
+
+        public bool alert_Checked { get; set; }
+    }
+}
