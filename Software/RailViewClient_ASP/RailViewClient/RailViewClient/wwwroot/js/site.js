01e31ab419d3e68e5ec4ccf42b9134ea11acb276
bendevriesxd@gmail.com <bendevriesxd@gmail.com>
2021-11-26 15:14:47 +0100

    Minor changes. Also included de Google Maps API (not working and is apparently not free to use)

--- Software/RailViewClient_ASP/RailViewClient/RailViewClient/wwwroot/js/site.js
+++ Software/RailViewClient_ASP/RailViewClient/RailViewClient/wwwroot/js/site.js
@@ -2,7 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var map;
+
 function ShowPopUp() {
     console.log("test");
     window.open('/Home/Privacy', "Live Feed", 'fullscreen="yes"');
+}
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
 }
