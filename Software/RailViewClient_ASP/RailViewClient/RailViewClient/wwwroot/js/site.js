17880b71be1ce9ab98afdbe69d2a469a60b902fa
Ben <bendevriesxd@gmail.com>
2021-11-29 23:39:40 +0100

    Map is visible in ASP using leaflet. Does need some styling

--- Software/RailViewClient_ASP/RailViewClient/RailViewClient/wwwroot/js/site.js
+++ Software/RailViewClient_ASP/RailViewClient/RailViewClient/wwwroot/js/site.js
@@ -2,16 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-var map;
 
 function ShowPopUp() {
     console.log("test");
     window.open('/Home/Privacy', "Live Feed", 'fullscreen="yes"');
 }
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
-}
+console.log("1");
+//load in map and map settings
+var map = L.map('map', {
+    center: [51.4993, 5.6570],
+    zoom: 8,
+    minZoom: 8,
+    maxZoom: 18,
+    maxBounds: [
+        [50.138758, 2.194824],
+        [53.921042, 7.863770]
+    ],
+    zoomControl: false
+});
+
+console.log("2");
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+console.log("3");
