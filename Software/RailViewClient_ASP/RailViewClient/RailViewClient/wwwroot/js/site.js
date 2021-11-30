8b966b06c136bd124c118eb40b7b15e8b1e68170
bendevriesxd@gmail.com <bendevriesxd@gmail.com>
2021-11-30 16:00:29 +0100

    Map is styled. Now trying to fix the coordinates obtained from the NS API

--- Software/RailViewClient_ASP/RailViewClient/RailViewClient/wwwroot/js/site.js
+++ Software/RailViewClient_ASP/RailViewClient/RailViewClient/wwwroot/js/site.js
@@ -8,24 +8,45 @@ function ShowPopUp() {
     window.open('/Home/Privacy', "Live Feed", 'fullscreen="yes"');
 }
 
-console.log("1");
 //load in map and map settings
 var map = L.map('map', {
     center: [51.4993, 5.6570],
-    zoom: 8,
-    minZoom: 8,
+    zoom: 7,
+    minZoom: 7,
     maxZoom: 18,
-    maxBounds: [
-        [50.138758, 2.194824],
-        [53.921042, 7.863770]
-    ],
+    //maxBounds: [
+    //    [50.138758, 2.194824],
+    //    [53.921042, 7.863770]
+    //],
     zoomControl: false
 });
 
-console.log("2");
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+var latlngs = [
+    [5.89918, 51.98506],
+    [5.89965, 51.985],
+    [5.90108, 51.98478],
+    [5.90187, 51.98468],
+    [5.90238, 51.98464],
+    [5.90346, 51.98464],
+    [5.90476, 51.98477],
+    [5.91167, 51.98567],
+    [5.91245, 51.98574],
+    [5.91316, 51.98579],
+    [5.9139, 51.98581],
+    [5.9146, 51.98582],
+    [5.91535, 51.98579],
+    [5.9161, 51.98574],
+    [5.91741, 51.9856],
+    [5.91821, 51.98547],
+    [5.91889, 51.98535],
+    [5.91941, 51.98523]
+];
 
-console.log("3");
+var polyline = L.polyline(latlngs, { color: 'blue' }).addTo(map);
+
+// zoom the map to the polyline
+map.fitBounds(polyline.getBounds());
