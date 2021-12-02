e43126559f25b8436507eccc389b5577d13e07ad
Nick Welles <nickwelles@live.nl>
2021-12-02 15:36:02 +0100

    started errorhandling

---
+++ Software/RailView_database_GUI/RailData/RailData/Models/ErrorHandling.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace RailData.Models
+{
+    public class ErrorHandling
+    {
+        public string ErrorType { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Error()
+        {
+            string completeError = "<p class='mt-3 alert alert-" + ErrorType + "' role='alert'>" + ErrorMessage + "</p>";
+
+            return completeError;
+        }
+    }
+}
