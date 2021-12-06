e36b59a3c6b51f77bf0467ec093f316caa202ff0
Nick Welles <nickwelles@live.nl>
2021-12-06 15:48:06 +0100

    add complete errorhandling. start at making dashboard screen

--- Software/RailView_database_GUI/RailData/RailData/Models/ErrorHandling.cs
+++ Software/RailView_database_GUI/RailData/RailData/Models/ErrorHandling.cs
@@ -1,16 +1,12 @@
-﻿using System.Collections.Generic;
-
-namespace RailData.Models
+﻿namespace RailData.Models
 {
     public class ErrorHandling
     {
-        public string ErrorType { get; set; }
         public string ErrorMessage { get; set; }
+
         public string Error()
         {
-            string completeError = "<p class='mt-3 alert alert-" + ErrorType + "' role='alert'>" + ErrorMessage + "</p>";
-
-            return completeError;
+            return ErrorMessage;
         }
     }
 }
