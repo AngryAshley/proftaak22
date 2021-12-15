4863d366b28ab1c078a41cd4f4fa035c0247db03
Nick Welles <nickwelles@live.nl>
2021-12-15 13:21:52 +0100

    changes to serverside socket script with ssl

--- Infra/RailViewServer.py
+++ Infra/RailViewServer.py
@@ -7,16 +7,10 @@ import mysql.connector
 ts = time.time()
 timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') # get the current timestamp
 
-context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 context.load_cert_chain(certfile="cert.pem", keyfile="cert.pem")
 
-s = socket.socket()
 buffer_size = 100
-
-sslsoc = context.wrap_socket(s)
-sslsoc.bind(('192.168.161.205', 6023))
-sslsoc.listen(5)
-
 doOnce = 0
 
 mydb = mysql.connector.connect(
@@ -37,35 +31,38 @@ def updateDatabase(type): # function to update database on type of alert
   railviewdata.execute(updateSqlTrain, valuesTrain)
   mydb.commit()
 
-while True:
-  newsocket, fromaddr = sslsoc.accept()
-  request = sslsoc.read()
-  print(request)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
+  sock.bind(('192.168.161.205', 6023))
+  sock.listen(5)
+  with context.wrap_socket(sock, server_side=True) as ssock:
+    while (True):
+      conn, addr = ssock.accept()
 
-  if doOnce == 0:
-    print('Got connection from ', fromaddr)
-    doOnce = 1
+      if doOnce == 0:
+        print('Got connection from ', addr)
+        doOnce = 1
 
-  data = newsocket.recv(buffer_size).decode('ascii')
+      data = conn.recv(buffer_size).decode()
+      print(data)
 
-  if data == "PERSON":
-    # select the id where the location = alert location send by the PI
-    # counter = counter + 1
-    print(data)
-    updateDatabase("person")
-    
-    #c.send(b'RECIEVED MESSAGE') #send data back to PI
+      if data == "PERSON":
+        # select the id where the location = alert location send by the PI
+        # counter = counter + 1
+        print(data)
+        updateDatabase("person")
+        
+        #c.send(b'RECIEVED MESSAGE') #send data back to PI
 
-  if data == "All clear":
-    print(data)
-    updateDatabase("other")
+      if data == "All clear":
+        print(data)
+        updateDatabase("other")
 
-  if data == "TRAIN":
-    print(data)
-    updateDatabase("train")
-    
-  if data == "!":
-    print('Closing...', fromaddr)
-    doOnce = 0
-    newsocket.close()
-    #break #close the script after recieving the closing message
+      if data == "TRAIN":
+        print(data)
+        updateDatabase("train")
+        
+      if data == "!":
+        print('Closing...', addr)
+        doOnce = 0
+        ssock.close()
+        #break #close the script after recieving the closing message
