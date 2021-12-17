78af918bf0d592e8cbd3eeed21a7cbfde6a0ab68
Ben <bendevriesxd@gmail.com>
2021-12-17 14:23:18 +0100

    Started working on a minimal web API

---
+++ Software/RailViewApi/RailViewApi/RailViewApi/NotificationContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RailViewApi
+{
+    public class NotificationContext : DbContext
+    {
+        public NotificationContext(DbContextOptions<NotificationContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Notification> Alerts { get; set; }
+    }
+
+    public class Notification
+    {
+        public int Id { get; set; }
+
+        public int Cam_Id { get; set; }
+
+        public string Alert { get; set; }
+
+        public double Location_X { get; set; }
+
+        public double Location_Y { get; set; }
+
+        public string Route { get; set; }
+
+        public string Times { get; set; }
+
+        public bool Alert_Checked { get; set; }
+    }
+}
